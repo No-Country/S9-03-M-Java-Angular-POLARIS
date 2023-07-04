9609b71354ddb19f3f9239fa8eed1c9ec496d889
Jafet Vazquez <jetvax.jv@gmail.com>
2023-07-04 10:29:40 -0600

    Landing Page component

--- client/src/app/public/public.module.ts
+++ client/src/app/public/public.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
     PublicComponent,
-    HomeComponent
+    HomeComponent,
+    LandingPageComponent
   ],
   imports: [
     CommonModule,
