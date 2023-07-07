d39a7fa41a79972abf1bafa432e876b53799aacf
ccama22 <ccamacruz2018@gmail.com>
2023-07-07 10:02:25 -0500

    data form v5

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
-
-
-
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     CommonModule
