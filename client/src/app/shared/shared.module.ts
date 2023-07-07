42e40eeab04f4f3054f6b46a2b9953bb2488b4d5
Federico Di Cillo <dicillo.fe@gmail.com>
2023-07-07 10:21:24 -0300

    Revert "Revert "Recuperar contraseña"" arreglando merge mal hecho This reverts commit 184fc38e8687cc4568fef2ab40668a4c8d765295.

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    NavbarComponent,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule { }
