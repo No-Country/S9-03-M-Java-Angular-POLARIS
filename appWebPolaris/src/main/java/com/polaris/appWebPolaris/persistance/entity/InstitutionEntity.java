e33a462229633d49f0da7414709ed58f0a76eaa9
LilianaGallego <lilianagallegom@gmail.com>
2023-07-13 19:01:55 -0500

    Se crea la entidad institucion

--- appWebPolaris/src/main/java/com/polaris/appWebPolaris/persistance/entity/InstitutionEntity.java
+++ appWebPolaris/src/main/java/com/polaris/appWebPolaris/persistance/entity/InstitutionEntity.java
@@ -1,7 +1,28 @@
 package com.polaris.appWebPolaris.persistance.entity;
 
-public class InstitutionEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "institutions")
+public class InstitutionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cuit;
+    private String email;
+    private String password;
+    private String province;
+    private String locality;
+    private String type;
+    private String availability;
+    private String numberphone;
+
+    private String rol;
 
 
 }
