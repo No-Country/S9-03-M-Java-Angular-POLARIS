9bb62b6ece251ac00f38ed0d74cfcba2ef5a0294
Federico Di Cillo <dicillo.fe@gmail.com>
2023-07-25 23:42:40 -0300

    edit institution

--- client/src/app/private/institution/institution-dashboard/institution-dashboard.component.ts
+++ client/src/app/private/institution/institution-dashboard/institution-dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit,ViewChild  } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators,ValidatorFn  } from '@angular/forms';
 import { CountryFormatServiceService } from 'src/app/public/landing-page/services/country-format-service.service';
+import { Institution } from 'src/app/shared/models/user/Institution';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { InstitutionService } from 'src/app/shared/services/institution.service';
 import { UserService } from 'src/app/shared/services/user.service';
+
 
 
 
@@ -12,11 +15,13 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./institution-dashboard.component.css']
 })
 export class InstitutionDashboardComponent {
+
+editState: Boolean = false
 instData: any | null = null;
 institution: any;
 dataToken: any;
 
-constructor(private authService: AuthService, private userService: UserService){}
+constructor(private authService: AuthService, private userService: UserService, private institutionService: InstitutionService){}
 
 ngOnInit():void{
 this.getInstitutionData();
@@ -36,7 +41,7 @@ getInstitutionData(){
       this.userService.getInstitutionById(Id, token).subscribe({
         next: (instData) => {
           this.institution = instData;
-          console.log("al fondo hay sitio",this.institution.name)
+          console.log("al fondo hay sitio",this.institution)
         },
         error: (error) => {
           console.error('Error al obtener el usuario:', error);
@@ -44,6 +49,46 @@ getInstitutionData(){
       });
     }
   }
+}
+
+
+editInstitution(){
+  const datosActualizados: Institution = {
+    id: this.institution.id,
+    name: this.institution.name,
+    cuit: this.institution.cuit,//
+    email: this.institution.email,
+    password: this.institution.password,//
+    province: this.institution.province,
+    locality: this.institution.locality,
+    type: this.institution.type,
+    availability: this.institution.availability,
+    numberphone: this.institution.numberphone,
+    imageProfile: this.institution.imageProfile,
+    rol: this.institution.rol//
+  };
+  const token = localStorage.getItem('token');
+  this.dataToken=token;
+  if(token){
+  this.institutionService.updateInstitucion(datosActualizados,token)
+    .subscribe(
+      response => {
+        // Manejo de la respuesta del servidor, si es necesario.
+        console.log('Institución actualizada:', response);
+      },
+      error => {
+        // Manejo de errores, si es necesario.
+        console.error('Error al actualizar la institución:', error);
+      }
+    );
+}
+this.editState = false;
+}
+
+
+
+editar(){
+  this.editState = true;
 }
 
 
--- client/src/app/shared/models/user/Institution.ts
+++ client/src/app/shared/models/user/Institution.ts
@@ -1,14 +1,14 @@
 export class Institution{
-    
-        name!: string;
-        cuit!: string;
-        email!: string;
-        password!: string;
-        province!:string;
-        locality!:string;
-        type!: string;
+        id!: number;
+        name?: string;
+        cuit?: string;
+        email?: string;
+        password?: string;
+        province?:string;
+        locality?:string;
+        type?: string;
         availability?: string;
         numberphone?: string;
         imageProfile?: string;
-        rol!: string;
+        rol?: string;
       }
