b709772c4310e752301e9612cedad7231732dfe5
LilianaGallego <lilianagallegom@gmail.com>
2023-07-26 11:26:48 -0500

    Correccion updateInstitution

--- appWebPolaris/src/main/java/com/polaris/appWebPolaris/domain/service/InstitutionService.java
+++ appWebPolaris/src/main/java/com/polaris/appWebPolaris/domain/service/InstitutionService.java
@@ -6,6 +6,7 @@ import com.polaris.appWebPolaris.domain.repository.IInstitutionRepository;
 import com.polaris.appWebPolaris.domain.useCase.IInstitutionUseCase;
 import com.polaris.appWebPolaris.exception.CustomerExistsException;
 import com.polaris.appWebPolaris.exception.EmailValidationException;
+import com.polaris.appWebPolaris.persistance.entity.InstitutionEntity;
 import com.polaris.appWebPolaris.security.Roles;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -67,12 +68,47 @@ public class InstitutionService implements IInstitutionUseCase {
 
     @Override
     public Optional<InstituteDto> update(InstituteDto modifyInstitute) {
-        if (iInstitutionRepository.getInstitutionById(modifyInstitute.getId()).isEmpty()){
+        Optional<InstituteDto> institutionBD = iInstitutionRepository.getInstitutionById(modifyInstitute.getId());
+        if (institutionBD.isEmpty()){
             return Optional.empty();
         }
-        modifyInstitute.setPassword(passwordEncoder.encode(modifyInstitute.getPassword()));
-        modifyInstitute.setRol(String.valueOf(Roles.INSTITUTION));
-        return Optional.of(iInstitutionRepository.save(modifyInstitute));
+        if (modifyInstitute.getName()!= null){
+            institutionBD.get().setName(modifyInstitute.getName());
+        }
+        if (modifyInstitute.getCuit()!= null){
+            institutionBD.get().setCuit(modifyInstitute.getCuit());
+        }
+        if (modifyInstitute.getEmail()!= null){
+            institutionBD.get().setEmail(modifyInstitute.getEmail());
+        }
+        if (modifyInstitute.getPassword()!= null){
+            institutionBD.get().setPassword(passwordEncoder.encode(modifyInstitute.getPassword()));
+        }
+        if (modifyInstitute.getProvince()!= null){
+            institutionBD.get().setProvince(modifyInstitute.getProvince());
+        }
+
+        if (modifyInstitute.getLocality()!= null){
+            institutionBD.get().setLocality(modifyInstitute.getLocality());
+        }
+        if (modifyInstitute.getType()!= null){
+            institutionBD.get().setType(modifyInstitute.getType());
+        }
+        if (modifyInstitute.getAvailability()!= null){
+            institutionBD.get().setAvailability(modifyInstitute.getAvailability());
+        }
+        if (modifyInstitute.getNumberphone()!= null){
+            institutionBD.get().setNumberphone(modifyInstitute.getNumberphone());
+        }
+        if (modifyInstitute.getImageProfile()!= null){
+            institutionBD.get().setImageProfile(modifyInstitute.getImageProfile());
+        }
+        if (modifyInstitute.getReviewList() != null){
+            institutionBD.get().setReviewList(modifyInstitute.getReviewList());
+        }
+
+        institutionBD.get().setRol(String.valueOf(Roles.INSTITUTION));
+        return Optional.of(iInstitutionRepository.save(institutionBD.get()));
     }
 
     @Override
